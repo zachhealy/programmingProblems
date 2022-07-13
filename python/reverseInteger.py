458e0c678dc2ccc9954076e7becdd0dc97e622bf
Zach Healy <73858595+zachhealy@users.noreply.github.com>
2022-07-13 10:11:49 -0400

    Update reverseInteger.py

--- python/reverseInteger.py
+++ python/reverseInteger.py
@@ -1,6 +1,6 @@
-#Difficulty: Medium
-
 #7. Reverse Integer
+
+#Difficulty: Medium
 
 #Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the
 #signed 32-bit integer range [-2^31, 2^31 - 1], then return 0.
