4439173ae556263c780964b6898dfa053dba613c
Zach Healy <73858595+zachhealy@users.noreply.github.com>
2022-07-13 10:25:04 -0400

    Update fizzBuzz.py

--- python/newUserChallenges/fizzBuzz.py
+++ python/newUserChallenges/fizzBuzz.py
@@ -1,31 +1,30 @@
-#412. Fizz Buzz
+""""
+412. Fizz Buzz
 
-#Difficulty: Easy
+Difficulty: Easy
 
-#Given an integer n, return a string array answer (1-indexed) where:
+Given an integer n, return a string array answer (1-indexed) where:
 
-#answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
-#answer[i] == "Fizz" if i is divisible by 3.
-#answer[i] == "Buzz" if i is divisible by 5.
-#answer[i] == i (as a string) if none of the above conditions are true.
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i (as a string) if none of the above conditions are true.
 
-#Example 1:
-#--------------------------------
-#Input: n = 3
-#Output: ["1","2","Fizz"]
-#Example 2:
+Example 1:
+Input: n = 3
+Output: ["1","2","Fizz"]
 
-#Input: n = 5
-#Output: ["1","2","Fizz","4","Buzz"]
-#Example 3:
+Example 2:
+Input: n = 5
+Output: ["1","2","Fizz","4","Buzz"]
 
-#Input: n = 15
-#Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+Example 3:
+Input: n = 15
+Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
  
- 
-#Constraints:
-#1 <= n <= 104
-
+Constraints:
+1 <= n <= 104
+""""
 
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
