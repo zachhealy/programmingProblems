99571bf0fb2476969cbc2ff72e9fd72566c2f6eb
zachhealy <zachjhealy@gmail.com>
2023-02-03 15:33:41 -0600

    Update zeroMatrix.py

--- CS455/textbook/zeroMatrix.py
+++ CS455/textbook/zeroMatrix.py
@@ -5,45 +5,22 @@ Write an algorithm such that if an element in an MxN matrix is 0,
 its entire row and column are set to 0.
 
 '''
-
 def zeroMatrix(matrix):
-    
     row = []
     col = []
     
     for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
+        for j in range(len(matrix[i])):
             if matrix[i][j] == 0:
                 row.append(i)
                 col.append(j)
                 
-    for x in range(len(matrix)):
-        matrix[x][j] = 0
-    for y in range(len(matrix[0])):
-        matrix[i][y] = 0                  
-    return matrix
-
-
-
-
-
-
-
-
-''' 
     for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            if matrix[i][j] == 0:
-                column.append(j)
-                row.append(i)
-                
-    for i in range(len(matrix)):
-        for j in column:
-            matrix[i][j] == 0
-    for i in row:
-        for j in range(len(matrix[0])):
-            matrix[i][j] == 0
-'''
+        for j in range(len(matrix[i])):
+            if i in row or j in col:
+                matrix[i][j] = 0
+                    
+                    
 test = [[1,1,1],[1,0,1],[1,1,1]]
 test2 = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
 
